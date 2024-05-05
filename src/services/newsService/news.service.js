d58ec389a6b776758d47ec297538640553e41f49
ljohnston00 <99911831+ljohnston00@users.noreply.github.com>
2024-05-04 21:47:46 -0400

    Update news.service.js

--- src/services/newsService/news.service.js
+++ src/services/newsService/news.service.js
@@ -1,27 +1,33 @@
 import Cookies from 'js-cookie';
 
 export const fetchMarketNews = async () => {
-  const token = Cookies.get('Authorization');
-  const response = await fetch("https://moneymarket.up.railway.app/news/market-news", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    },
-    body: JSON.stringify({
-        language: 'en', 
-      country: 'us', 
-      limit: 10, 
-      publishedAfterDate: '2024-03-01', 
-      tickers: ['AAPL', 'GOOG'] 
-    })
-  });
+  try {
+    const token = Cookies.get('Authorization');
+    const response = await fetch("https://moneymarket.up.railway.app/news/market-news", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({
+          language: 'en', 
+        country: 'us', 
+        limit: 10, 
+        publishedAfterDate: '2024-03-01', 
+        tickers: ['AAPL', 'GOOG'] 
+      })
+    });
 
-  if (response.ok) {
+    if (!response.ok) {
+      const error = new Error('Failed to fetch market news');
+      error.status = response.status;
+      throw error;
+    }
+
     const data = await response.json();
     return data;
-  } else {
-    console.log('Failed to fetch market news');
-    return [];
+  } catch(err) {
+    console.log(err);
+    throw err;
   }
 };
