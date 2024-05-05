ee78f04d4e9b60bf43bdc103312cd8b7681565ae
ljohnston00 <99911831+ljohnston00@users.noreply.github.com>
2024-05-04 21:46:06 -0400

    Update signin.service.js

--- src/services/auth/signin.service.js
+++ src/services/auth/signin.service.js
@@ -1,34 +1,34 @@
-import Cookies from 'js-cookie';
-import { ApiError } from "../error.service";
 import Paths from "../path.service";
+import Cookies from "js-cookie";
 
 const signinHandler = async (email, password) => {
-    try{
-    const signInResponse = await fetch(`${Paths.API_BASE}/auth/signin`, {
+    try {
+      const signInResponse = await fetch(`${Paths.API_BASE}/auth/signin`, {
         method: 'POST',
         headers: {
-        'Content-Type': 'application/json',
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-        "email": email,
-        "password": password
+          "email": email,
+          "password": password
         })
-    });
-
-    if(!signInResponse.ok){
-        throw new Error('Invalid email or password');
+      });
+  
+      if (!signInResponse.ok) {
+        const error = new Error('Invalid email or password');
+        error.status = signInResponse.status;
+        throw error;
+      }
+  
+      const signInData = await signInResponse.json();
+      Cookies.set("Authorization", signInData.access_token);
+      localStorage.setItem('isLoggedIn', 'true');
+      return true;
+    } catch (err) {
+      console.log(err);
+      throw err;
     }
-    const signInData = await signInResponse.json();
-    Cookies.set("Authorization", signInData.access_token);
-    localStorage.setItem('isLoggedIn', 'true');
-    return true;
-  } catch (err) {
-    console.log(err);
-        if(err.response.status === 401) {
-            return ApiError.UNAUTHORIZED;
-        }
-        return null;
-  }
-};
+  };
+  
 
 export default signinHandler;
