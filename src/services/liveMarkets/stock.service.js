1436ffc6ff6d25ac01ab743c8e67543683d87a5d
ljohnston00 <99911831+ljohnston00@users.noreply.github.com>
2024-05-04 21:47:29 -0400

    Update stock.service.js

--- src/services/liveMarkets/stock.service.js
+++ src/services/liveMarkets/stock.service.js
@@ -2,28 +2,34 @@ import Cookies from 'js-cookie';
 import Paths from "../path.service";
 
 export const fetchStockData = async (ticker, timeSpan, startDate, endDate) => {
-  const token = Cookies.get('Authorization');
-  const limit = '100';
-  const response = await fetch(`${Paths.API_BASE}/stockdata/ticker`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    },
-    body: JSON.stringify({
-        "tickerName": ticker.toLocaleUpperCase(),
-        "timeSpan": timeSpan, 
-        "startDate": startDate, 
-        "endDate": endDate, 
-        "limit": limit
-      })
-    });
-  
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else {
-        console.log('Failed to fetch stock data');
-    return [];
+  try {
+    const token = Cookies.get('Authorization');
+    const limit = '100';
+    const response = await fetch(`${Paths.API_BASE}/stockdata/ticker`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({
+          "tickerName": ticker.toLocaleUpperCase(),
+          "timeSpan": timeSpan, 
+          "startDate": startDate, 
+          "endDate": endDate, 
+          "limit": limit
+        })
+      });
+
+    if (!response.ok) {
+      const error = new Error('Failed to fetch stock data');
+      error.status = response.status;
+      throw error;
+    }
+
+    const data = await response.json();
+    return data;
+  } catch(err) {
+    console.log(err);
+    throw err;
   }
 };
